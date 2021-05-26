66ac3c1a71c40f6f79b3deb9d90317e48b8861a3
liuwei9 <1954729948@qq.com>
2021-05-26 21:41:25 +0800

    create

---
+++ proj/DMA_CTRL.srcs/sources_1/new/DMA_READ_CTRL.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/05/26 21:34:59
+// Design Name: 
+// Module Name: DMA_READ_CTRL
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module DMA_READ_CTRL(
+
+    );
+endmodule
